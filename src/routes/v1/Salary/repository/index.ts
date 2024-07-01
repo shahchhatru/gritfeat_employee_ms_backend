cb79044adf3387ed6588b55038edcdc1b7cd7907
shahchhatru <shahkhesavchhatra@gmail.com>
2024-07-01 11:58:21 +0545

    changes in repo

--- src/routes/v1/Salary/repository/index.ts
+++ src/routes/v1/Salary/repository/index.ts
@@ -1,8 +1,9 @@
-import { SalaryModel, SalaryDocument } from '../../../models/salary';
+import { SalaryModel, SalaryDocument } from '../../../../models/salary';
 import { Salary } from '../../../../types/salary';
 
 export const createSalary = (salary: Salary): Promise<SalaryDocument> => {
     const sal = new SalaryModel(salary);
     return sal.save();
 }
+
 
