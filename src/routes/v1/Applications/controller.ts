150c8db801250c1f2d859b901e7eb1f6550912cf
shahchhatru <shahkhesavchhatra@gmail.com>
2024-07-03 11:47:08 +0545

    application controller redis

--- src/routes/v1/Applications/controller.ts
+++ src/routes/v1/Applications/controller.ts
@@ -3,7 +3,7 @@ import { successResponse } from '../../../utils/HttpResponse';
 import { messages } from '../../../utils/Messages';
 import ApplicationService from './service';
 import CustomError from '../../../utils/Error';
-
+import { redisClient } from '../../../config/redisConfig'; // Import the Redis client
 
 const ApplicationController = {
     async createApplication(req: Request, res: Response, next: NextFunction) {
@@ -13,12 +13,17 @@ const ApplicationController = {
             if (!user._id) throw new CustomError(messages.user.user_not_found, 404)
             const body = req.body;
             const result = await ApplicationService.createApplication({ ...body, user: user._id, organization: user.organizationId });
+
+            // Invalidate the cache for all applications for the organization
+            const cacheKey = `applications:organization:${user.organizationId}`;
+            await redisClient.del(cacheKey);
+
             return successResponse({
                 response: res,
                 message: messages.application.creation_success,
                 data: result,
                 status: 201
-            })
+            });
         } catch (error) {
             next(error);
         }
@@ -27,55 +32,63 @@ const ApplicationController = {
     async updateStatus(req: Request, res: Response, next: NextFunction) {
         try {
             const id = req.params.id;
-            // console.log(req.params)
             const user = res.locals.user;
             if (!user) throw new CustomError(messages.user.user_not_found, 404)
             if (!user._id) throw new CustomError(messages.user.user_not_found, 404)
-            // console.log(`req.body`, req.body);
-            // console.log(`user`, user);
-            if (user.role !== 'SUPERVISOR') {
-                if (user.role !== 'ADMIN') {
-                    throw new CustomError(messages.application.edit_forbidden, 404);
-                }
-
+            if (user.role !== 'SUPERVISOR' && user.role !== 'ADMIN') {
+                throw new CustomError(messages.application.edit_forbidden, 404);
             }
-            // console.log(`req.user`, user);
+
             const body = req.body;
             const application = await ApplicationService.getApplicationById(id);
-            if (application.supervisor?.toString() !== user._id) throw new CustomError(messages.user.user_not_found, 404)
+            if (application.supervisor?.toString() !== user._id) throw new CustomError(messages.user.user_not_found, 404);
+
             const result = await ApplicationService.updateApplication(id, { status: body.status });
+
+            // Invalidate the cache for the specific application and all applications for the organization
+            const cacheKey = `application:${id}`;
+            await redisClient.del(cacheKey);
+            const orgCacheKey = `applications:organization:${user.organizationId}`;
+            await redisClient.del(orgCacheKey);
+
             return successResponse({
                 response: res,
                 message: messages.application.edit_success,
                 data: result,
                 status: 200
-            })
+            });
         } catch (error) {
             next(error);
         }
     },
-
 
     async editApplication(req: Request, res: Response, next: NextFunction) {
         try {
             const user = res.locals.user;
             if (!user) throw new CustomError(messages.user.user_not_found, 404)
             if (!user._id) throw new CustomError(messages.user.user_not_found, 404)
-            // if (user.role !== 'supervisor') throw new CustomError(messages.application.edit_forbidden, 404)
+
             const body = req.body;
             const application = await ApplicationService.getApplicationById(body.id);
-            if (application.user?.toString() !== user._id) throw new CustomError(messages.application.edit_forbidden, 404)
+            if (application.user?.toString() !== user._id) throw new CustomError(messages.application.edit_forbidden, 404);
+
             const result = await ApplicationService.updateApplication(body.id, { ...body, user: user._id });
+
+            // Invalidate the cache for the specific application and all applications for the organization
+            const cacheKey = `application:${body.id}`;
+            await redisClient.del(cacheKey);
+            const orgCacheKey = `applications:organization:${user.organizationId}`;
+            await redisClient.del(orgCacheKey);
+
             return successResponse({
                 response: res,
                 message: messages.application.edit_success,
                 data: result,
                 status: 200
-            })
+            });
         } catch (error) {
             next(error);
         }
-
     },
 
     async deleteApplication(req: Request, res: Response, next: NextFunction) {
@@ -83,21 +96,28 @@ const ApplicationController = {
             const user = res.locals.user;
             if (!user) throw new CustomError(messages.user.user_not_found, 404)
             if (!user._id) throw new CustomError(messages.user.user_not_found, 404)
-            // if (user.role !== 'supervisor') throw new CustomError(messages.application.edit_forbidden, 404)
+
             const id = req.params.id;
             const application = await ApplicationService.getApplicationById(id);
-            if (application.user?.toString() !== user._id) throw new CustomError(messages.application.edit_forbidden, 404)
+            if (application.user?.toString() !== user._id) throw new CustomError(messages.application.edit_forbidden, 404);
+
             const result = await ApplicationService.deleteApplication(id);
+
+            // Invalidate the cache for the specific application and all applications for the organization
+            const cacheKey = `application:${id}`;
+            await redisClient.del(cacheKey);
+            const orgCacheKey = `applications:organization:${user.organizationId}`;
+            await redisClient.del(orgCacheKey);
+
             return successResponse({
                 response: res,
                 message: messages.application.delete_success,
                 data: result,
                 status: 200
-            })
+            });
         } catch (error) {
             next(error);
         }
-
     },
 
     async getApplicationByOrganizationId(req: Request, res: Response, next: NextFunction) {
@@ -106,35 +126,68 @@ const ApplicationController = {
             const user = res.locals.user;
             if (user.role !== 'ADMIN') throw new CustomError(messages.actions.forbidden_message, 404)
             if (user.organizationId.toString() !== id) throw new CustomError(messages.actions.forbidden_message, 404)
+
+            const cacheKey = `applications:organization:${id}`;
+
+            // Check if the data is in the cache
+            const cachedApplications = await redisClient.get(cacheKey);
+            if (cachedApplications) {
+                return successResponse({
+                    response: res,
+                    message: messages.application.all_get_success,
+                    data: JSON.parse(cachedApplications),
+                    status: 200
+                });
+            }
+
             const result = await ApplicationService.getApplicationByOrganizationId(id);
+
+            // Store the result in the cache
+            await redisClient.set(cacheKey, JSON.stringify(result), { EX: 3600 }); // Cache for 1 hour
+
             return successResponse({
                 response: res,
                 message: messages.application.all_get_success,
                 data: result,
                 status: 200
-            })
+            });
         } catch (error) {
             next(error);
         }
-
     },
+
     async getApplicationByOrganization(req: Request, res: Response, next: NextFunction) {
         try {
-
             const user = res.locals.user;
-            if (user.role !== 'ADMIN') throw new CustomError(messages.actions.forbidden_message, 404)
+            if (user.role !== 'ADMIN') throw new CustomError(messages.actions.forbidden_message, 404);
+
+            const cacheKey = `applications:organization:${user.organizationId}`;
+
+            // Check if the data is in the cache
+            const cachedApplications = await redisClient.get(cacheKey);
+            if (cachedApplications) {
+                return successResponse({
+                    response: res,
+                    message: messages.application.all_get_success,
+                    data: JSON.parse(cachedApplications),
+                    status: 200
+                });
+            }
 
             const result = await ApplicationService.getApplicationByOrganizationId(user.organizationId.toString());
+
+            // Store the result in the cache
+            await redisClient.set(cacheKey, JSON.stringify(result), { EX: 3600 }); // Cache for 1 hour
+
             return successResponse({
                 response: res,
                 message: messages.application.all_get_success,
                 data: result,
                 status: 200
-            })
+            });
         } catch (error) {
             next(error);
         }
-
     },
 
     async getApplicationBySupervisorId(req: Request, res: Response, next: NextFunction) {
@@ -142,37 +195,70 @@ const ApplicationController = {
             const id = req.params.id;
             const user = res.locals.user;
             if (user._id.toString() != id) throw new CustomError(messages.actions.forbidden_message, 404);
+
+            const cacheKey = `applications:supervisor:${id}`;
+
+            // Check if the data is in the cache
+            const cachedApplications = await redisClient.get(cacheKey);
+            if (cachedApplications) {
+                return successResponse({
+                    response: res,
+                    message: messages.application.all_get_success,
+                    data: JSON.parse(cachedApplications),
+                    status: 200
+                });
+            }
+
             const result = await ApplicationService.getApplicationBySupervisorId(id);
+
+            // Store the result in the cache
+            await redisClient.set(cacheKey, JSON.stringify(result), { EX: 3600 }); // Cache for 1 hour
+
             return successResponse({
                 response: res,
                 message: messages.application.all_get_success,
                 data: result,
                 status: 200
-            })
+            });
         } catch (error) {
             next(error);
         }
-    }
-    ,
+    },
 
     async getApplicationsByUserId(req: Request, res: Response, next: NextFunction) {
         try {
             const id = req.params.id;
             const user = res.locals.user;
             if (user._id.toString() != id) throw new CustomError(messages.actions.forbidden_message, 404);
+
+            const cacheKey = `applications:user:${id}`;
+
+            // Check if the data is in the cache
+            const cachedApplications = await redisClient.get(cacheKey);
+            if (cachedApplications) {
+                return successResponse({
+                    response: res,
+                    message: messages.application.all_get_success,
+                    data: JSON.parse(cachedApplications),
+                    status: 200
+                });
+            }
+
             const result = await ApplicationService.getApplicationByUserId(id);
+
+            // Store the result in the cache
+            await redisClient.set(cacheKey, JSON.stringify(result), { EX: 3600 }); // Cache for 1 hour
+
             return successResponse({
                 response: res,
                 message: messages.application.all_get_success,
                 data: result,
                 status: 200
-            })
+            });
         } catch (error) {
             next(error);
         }
     }
-
-
-}
+};
 
 export default ApplicationController;
